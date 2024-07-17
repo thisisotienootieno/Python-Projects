021ea40a2d8883ec504fea8b365efecd2911264d
Otieno Otieno <163847274+thisisotienootieno@users.noreply.github.com>
2024-07-17 11:11:03 +0300

    Rock Paper Scissors Game
    Rock Paper Scissors Game

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import random
 
-top_of_range = input("Enter a number: ")
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+user_wins = 0
+computer_wins = 0
 
-    random_number = random.randrange(top_of_range)
-
-else:
-    print("Enter a number!")
-
-guess = 0
+options = ["rock", "paper", "scissors"]
 
 while True:
-    guess += 1
-    guess_number = input("Guess a number: ")
-    if guess_number.isdigit():
-        guess_number = int(guess_number)
-    else:
-        print("Enter a number!")
-        continue
-
-    if guess_number == random_number:
-        print("Great! Correct Guess")
-
-        if guess == 1:
-            print(f"You got it after {guess} guess.")
-        else:
-            print(f"You got it after {guess} guesses.")
-
+    user_pick = input("Type Rock/Paper/Scissors or Q to Quit: ")
+    user_pick = user_pick.lower()
+    if user_pick == "q":
+        print("Good Bye! Game over!")
         break
-
-    elif guess_number > random_number:
-        print("Your guess is higher than the number.")
-
     else:
-        print("Your guess is lower than the number.")
+        if user_pick not in options:
+            print("Option Not Available!")
+            continue
+
+        random_number = random.randrange(3)
+
+        computer_pick = options[random_number]
+        print(f"Computer picked {computer_pick}")
+
+        if user_pick == "rock" and computer_pick == "scissors":
+            print("You Won!")
+            user_wins += 1
+
+        elif user_pick == "scissors" and computer_pick == "paper":
+            print("You Won!")
+            user_wins += 1
+
+        elif user_pick == "paper" and computer_pick == "rock":
+            print("You Won!")
+            user_wins += 1
+
+        elif user_pick == computer_pick:
+            print("Nobody Won!")
+
+        else:
+            print("You Lost!")
+            print("Computer Wins")
+            computer_wins += 1
+
+    print(f"You won {user_wins} times.")
+    print(f"Computer won {computer_wins} times.")
+    if computer_wins > user_wins:
+        print("Computer Wins.")
+    elif computer_wins == user_wins:
+        print("The game is a Draw!")
+    else:
+        print("You Win!")
+
+total_plays = user_wins + computer_wins
+user_percentage = (user_wins / total_plays) * 100
+print(f"The game was played {total_plays} times and the Player won {user_percentage}% of the times.")
